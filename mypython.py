fe66e825f36152b1a3206d183189909cbe2f05e8
chamee-cc <72635733+chamee-cc@users.noreply.github.com>
2020-10-10 08:29:08 +0530

    Update mypython.py
    settled if -elif statements

--- mypython.py
+++ mypython.py
@@ -4,8 +4,10 @@ if 9 < num < 99:
     print("This is a Two Digit Number")
 elif 99 < num < 999:
     print ("This is a Three Digit Number")
+elif 999 < num < 9999:
+    print ("This is a Four Digit Number")
 else:
-    print("Number is <= 9 or >= 999")
+    print("Number is <= 9 or >= 9999")
 
 
 
