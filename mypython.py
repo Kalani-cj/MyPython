358f2e24bbabd71abeba70e376fd083cd6bb7c09
Kalani <kalani.17@business.mrt.ac.lk>
2020-10-09 19:22:23 -0700

    Python first Commit

---
+++ mypython.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+num=100
+if 9 < num < 99:
+    print("This is a Two Digit Number")
+else:
+    print("This is not a Digit Number")
+
+
+# In[ ]:
+
+
+
+
